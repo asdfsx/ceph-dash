0e583131323d7d2f9dd7907e5f027cb60b2e81ed
asdfsx <asdfsx@gmail.com>
2015-11-03 18:52:22 +0800

    add osd perf command

--- app/disk/views.py
+++ app/disk/views.py
@@ -51,11 +51,35 @@ class CephClusterCommand(dict):
 
 ###################
 #
-#         ceph osd df result
-#    {u'nodes': [{u'kb': 99565040, u'name': u'osd.0', u'type_id': 0, u'reweight': 1.0, u'crush_weight': 0.089996, u'utilization': 0.169312, u'depth': 2, u'kb_avail': 99396464, u'kb_used': 168576, u'var': 0.927465, u'type': u'osd', u'id': 0}, {u'kb': 99565040, u'name': u'osd.1', u'type_id': 0, u'reweight': 1.0, u'crush_weight': 0.089996, u'utilization': 0.206579, u'depth': 2, u'kb_avail': 99359360, u'kb_used': 205680, u'var': 1.131602, u'type': u'osd', u'id': 1}, {u'kb': 99565040, u'name': u'osd.2', u'type_id': 0, u'reweight': 1.0, u'crush_weight': 0.089996, u'utilization': 0.204144, u'depth': 2, u'kb_avail': 99361784, u'kb_used': 203256, u'var': 1.118266, u'type': u'osd', u'id': 2}, {u'kb': 99565040, u'name': u'osd.3', u'type_id': 0, u'reweight': 1.0, u'crush_weight': 0.089996, u'utilization': 0.1821, u'depth': 2, u'kb_avail': 99383732, u'kb_used': 181308, u'var': 0.997513, u'type': u'osd', u'id': 3}, {u'kb': 99565040, u'name': u'osd.5', u'type_id': 0, u'reweight': 1.0, u'crush_weight': 0.089996, u'utilization': 0.150635, u'depth': 2, u'kb_avail': 99415060, u'kb_used': 149980, u'var': 0.825154, u'type': u'osd', u'id': 5}], 
+#ceph osd df result
+#    {u'nodes': [{u'kb': 99565040, u'name': u'osd.0', u'type_id': 0, u'reweight': 1.0, u'crush_weight': 0.089996, u'utilization': 0.169312, 
+#                 u'depth': 2, u'kb_avail': 99396464, u'kb_used': 168576, u'var': 0.927465, u'type': u'osd', u'id': 0}, 
+#                 {u'kb': 99565040, u'name': u'osd.1', u'type_id': 0, u'reweight': 1.0, u'crush_weight': 0.089996, u'utilization': 0.206579, 
+#                  u'depth': 2, u'kb_avail': 99359360, u'kb_used': 205680, u'var': 1.131602, u'type': u'osd', u'id': 1}, 
+#                 {u'kb': 99565040, u'name': u'osd.2', u'type_id': 0, u'reweight': 1.0, u'crush_weight': 0.089996, u'utilization': 0.204144, 
+#                  u'depth': 2, u'kb_avail': 99361784, u'kb_used': 203256, u'var': 1.118266, u'type': u'osd', u'id': 2}, 
+#                 {u'kb': 99565040, u'name': u'osd.3', u'type_id': 0, u'reweight': 1.0, u'crush_weight': 0.089996, u'utilization': 0.1821, 
+#                  u'depth': 2, u'kb_avail': 99383732, u'kb_used': 181308, u'var': 0.997513, u'type': u'osd', u'id': 3}, 
+#                 {u'kb': 99565040, u'name': u'osd.5', u'type_id': 0, u'reweight': 1.0, u'crush_weight': 0.089996, u'utilization': 0.150635, 
+#                  u'depth': 2, u'kb_avail': 99415060, u'kb_used': 149980, u'var': 0.825154, u'type': u'osd', u'id': 5}], 
 #     u'stray': [], 
-#     u'summary': {u'total_kb': 497825200, u'dev': 0.021155, u'max_var': 1.131602, u'total_kb_avail': 496916400, u'min_var': 0.825154, u'average_utilization': 0.182554, u'total_kb_used': 908800}
+#     u'summary': {u'total_kb': 497825200, u'dev': 0.021155, u'max_var': 1.131602, u'total_kb_avail': 496916400, u'min_var': 0.825154, 
+#                  u'average_utilization': 0.182554, u'total_kb_used': 908800}
 #    }
+#
+#
+#ceph osd perf result
+#    {u'osd_perf_infos': 
+#     [{u'id': 0, u'perf_stats': {u'apply_latency_ms': 64, u'commit_latency_ms': 34}}, 
+#      {u'id': 1, u'perf_stats': {u'apply_latency_ms': 53, u'commit_latency_ms': 30}}, 
+#      {u'id': 2, u'perf_stats': {u'apply_latency_ms': 7, u'commit_latency_ms': 4}}, 
+#      {u'id': 3, u'perf_stats': {u'apply_latency_ms': 21, u'commit_latency_ms': 11}}, 
+#      {u'id': 5, u'perf_stats': {u'apply_latency_ms': 255, u'commit_latency_ms': 75}}]}
+#
+#
+#ceph osd maxosd result
+#    {u'epoch': 190, u'max_osd': 6}
+#
 #
 ###################
 
@@ -85,15 +109,30 @@ class DisksResource(ApiResource):
                 abort(500, disk_status['err'])
             summary = disk_status['summary']
  
-            headline = []
-            nodes = []
-            for key in disk_status['nodes'][0]:
-                headline.append(key)
-            nodes.append(headline)
+            disk_perf = CephClusterCommand(cluster, prefix='osd perf', format='json')
+            if 'err' in disk_perf:
+                abort(500, disk_perf['err'])
+
+            disk_maxosd = CephClusterCommand(cluster, prefix='osd getmaxosd', format='json')
+            if 'err' in disk_maxosd:
+                abort(500, disk_maxosd['err'])
+            summary['max_osd'] = disk_maxosd['max_osd']
+            summary['epoch'] = disk_maxosd['epoch']
+
+            nodes = {}
+            title = ['name','var','crush_weight','utilization','kb_used','kb_avail','kb',]
             for node in disk_status['nodes']:
-                tmp = []
-                for k in headline:
-                    tmp.append(node[k])
-                nodes.append(tmp)
-            return render_template('disks.html', summary=summary, nodes=nodes, config=self.config)
+                tmp = {}
+                for t in title:
+                    tmp[t] = node[t]
+                nodes[node['id']] = tmp
+            
+            title.append('apply_latency_ms')
+            title.append('commit_latency_ms')
+            for perf in disk_perf['osd_perf_infos']:
+                if perf['id'] in nodes:
+                    nodes[perf['id']]['apply_latency_ms'] = perf['perf_stats']['apply_latency_ms']
+                    nodes[perf['id']]['commit_latency_ms'] = perf['perf_stats']['commit_latency_ms']
+
+            return render_template('disks.html', summary=summary, nodes=nodes, title=title, config=self.config)
 
