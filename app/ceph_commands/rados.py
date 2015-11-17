aec56e918610e0bd22c55c790881c578a4059b1e
asdfsx <asdfsx@gmail.com>
2015-11-17 20:18:49 +0800

    add "rados" command in ceph_commands

---
+++ app/ceph_commands/rados.py
@@ -0,0 +1,32 @@
+#!/usr/env/bin python
+#! -*- coding: utf-8 -*-
+
+from . import base
+
+class rados(base.CommandExecutor):
+    def __init__(self):
+        self.prefix = "rados "
+        self.cmdmap = {"ls":"ls --pool %s --format json",
+                       }
+
+
+    def generate(self, cmdname, **para):
+        if cmdname == "ls":
+            if 'pool' in para :
+                return True, self.prefix + self.cmdmap[cmdname] % (para['pool'],)
+            else:
+                return False, "need 'pool' in parameters"
+        else:
+            return True, self.prefix + self.cmdmap[cmdname]
+
+    def execute(self, cmdname, **para):
+        isSuccess, result = self.generate(cmdname, **para)
+        print result
+        if isSuccess:
+            return True, self.executeCmd(result)
+        else:
+            return False, result
+       
+    def __call__(self, cmdname, **para):
+        return self.execute(cmdname, **para)
+
